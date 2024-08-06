6c3e8fc8929939fc8ce2c7b177355fa35501f38f
juliette maloke <your@email.com>
2024-08-06 13:54:55 -0500

    Update database connection configuration

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,21 @@
-// config/connection.js
+// // config/connection.js
+// const mongoose = require('mongoose');
+// require('dotenv').config(); // Load environment variables from .env file
+
+// function connectDb() {
+//   const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialDB';
+//   mongoose.connect(dbURI)
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(error => console.error('MongoDB connection error:', error));
+// }
+
+// module.exports = connectDb;
+
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
 function connectDb() {
-  const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialDB';
+  const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB'; // Ensure this matches the existing database name
   mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('MongoDB connection error:', error));
